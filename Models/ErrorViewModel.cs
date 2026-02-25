e9ec50c35cd5047efc85eea1fab91e54efa03e3d
Aklilu Abera <akililuabera44@gmail.com>
2026-02-25 17:11:22 +0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace OnlineTicketManagementSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
