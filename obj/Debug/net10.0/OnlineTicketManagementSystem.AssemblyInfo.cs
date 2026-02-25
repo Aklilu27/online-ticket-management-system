9a2aeabdb0b2abde25b836fdc9fcb7e2d3cab29f
Aklilu Abera <akililuabera44@gmail.com>
2026-02-25 17:59:18 +0300

    Add EF Core SqlServer and connection string
    Add Microsoft.EntityFrameworkCore.SqlServer (v10.0.3) to the project and add a DefaultConnection connection string in appsettings.json for SupportCenterDB. Add a new Data/ApplicationDbContext.cs file (DbContext placeholder). Project artifacts (obj/*, project.assets.json, assembly info) were updated as a result of the package restore.

--- obj/Debug/net10.0/OnlineTicketManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/OnlineTicketManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineTicketManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ec50c35cd5047efc85eea1fab91e54efa03e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineTicketManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineTicketManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
